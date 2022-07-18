2b9a43fb5acc4460e20d4568b97b509f69348273
Trent Cooper <00trent@protonmail.com>
2022-07-18 12:33:22 -0400

    Added capability to query and compute flag error rate percentage + evaluation against error threshold rules
    Co-authored-by: Elaine Vuong <e.vuong94@gmail.com>
    Co-authored-by: Steven Liou <stevenliou@gmail.com>
    Co-authored-by: Jordan Eggers <jordan.shay.eggers@gmail.com>

---
+++ index.js
@@ -0,0 +1,26 @@
+const CircuitManager = require('./lib/circuitManager');
+
+const config = {
+  server: 'nats://127.0.0.1:4222',
+  appId: 9,
+  redisAddress: '',
+};
+
+(async () => {
+  const manager = new CircuitManager(config);
+  await manager.initializeCircuit();
+  const circuitBreaker = manager.circuitBreaker;
+
+  setInterval(() => {
+    circuitBreaker.checkCircuits();
+  }, 5000);
+  console.log(manager.getActiveRules());
+
+  const cleanup = async () => {
+    await manager.disconnect();
+    console.log('closing nats');
+  };
+
+  process.on('SIGINT', cleanup);
+  process.on('SIGTERM', cleanup);
+})();
